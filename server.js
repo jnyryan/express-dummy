86e923cc10ba5dd1aaf57b0db6b1b8b0a928aaf4
John Ryan <jnyryan@gmail.com>
2019-06-27 16:36:19 +0100

    healthcheck

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('We are the music makers, we are the dreamers of the dreams!'))
+app.get('/healthcheck', (req, res) => res.sendStatus(200))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

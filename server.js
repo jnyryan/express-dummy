ac28145d2570c2e399a23a8330f056a404d90204
John Ryan <jnyryan@gmail.com>
2020-12-07 10:29:32 +0000

    Update server.js
    config port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('We are the music makers, we are the dreamers of the dreams!'))
 app.get('/healthcheck', (req, res) => res.sendStatus(200))
